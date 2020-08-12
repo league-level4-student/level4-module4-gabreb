b6f584153bf179d33d79a1c0e6eaa5b667f65ee5
gabreb <62906858+gabreb@users.noreply.github.com>
2020-08-12 12:27:31 -0700

    hi

---
+++ src/hisads.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class hisads {
+	public static void main(String[] args) {
+		String water = "water";
+		 String z = "";
+		 for(int i = 0; i < water.length(); i++){
+		     z += water.substring(water.length() - 1 - i, water.length() - i);
+		} 
+		System.out.println(z); 
+	 {
+		
+	}
+	
+	}
+}
